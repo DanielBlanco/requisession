bb86da9b61020ac46d96cdffbaa873ea475242c7
DanielBlanco <daniel.blancorojas@gmail.com>
2012-02-21 12:10:52 -0600

    Adding main plugin files

---
+++ requisession.php
@@ -0,0 +1,17 @@
+<?php
+/**
+* @version   $Id$
+* @package   Jumi
+* @copyright (C) 2008 - 2010 Martin Hajek, 2011 Edvard Ananyan
+* @license   GNU/GPL v3 http://www.gnu.org/licenses/gpl.html
+*/
+
+defined('_JEXEC') or die('Restricted access');
+
+// Import library dependencies
+jimport('joomla.plugin.plugin');
+jimport('joomla.event.plugin');
+
+class plgSystemRequisession extends JPlugin {
+
+}
